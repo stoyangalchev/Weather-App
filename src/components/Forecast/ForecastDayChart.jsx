847bbf5a1e9db9679893f1ba9c32752543646f69
stoyangalchev <galchev98@gmail.com>
2024-09-23 21:59:21 +0300

    Create ForecastDayChart.jsx

---
+++ src/components/Forecast/ForecastDayChart.jsx
@@ -0,0 +1,45 @@
+import { LABELS } from '../Const'
+import { Line } from 'react-chartjs-2'
+
+export default function ForecastDayChart({ data }) {
+  const data2 = {
+    labels: LABELS,
+    datasets: [
+      {
+        label: 'Temperature',
+        backgroundColor: '#EAC435',
+        borderColor: 'rgba(234, 196, 53,0.5)',
+        data
+      }
+    ]
+  }
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    aspectRatio: 3,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: 'white'
+        }
+      },
+      y: {
+        ticks: {
+          color: 'white'
+        }
+      }
+    }
+  }
+
+  return (
+    <div className='chart-container col-span-4 row-start-3  row-span-2'>
+      <Line options={options} data={data2} />
+    </div>
+  )
+}
